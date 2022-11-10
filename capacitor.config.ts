e8b013545b2c3f159ac83e1be12a7349fc895519
Olivier GBLEM <olivier@keographix.com>
2022-11-10 07:43:11 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.keogblem.taskin',
+  appName: 'TaskIn',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
